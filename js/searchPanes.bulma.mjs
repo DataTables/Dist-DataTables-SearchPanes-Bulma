7e5c704516924c6921ce77fb6c7035f552380994
Allan Jardine <allan@datatables.net>
2022-10-10 19:51:21 +0000

    Add support for mjs files (ES Modules)

---
+++ js/searchPanes.bulma.mjs
@@ -0,0 +1,24 @@
+
+/*! Bulma integration for DataTables' SearchPanes
+ * © SpryMedia Ltd - datatables.net/license
+ */
+
+import $ from 'jquery';
+import DataTable from 'datatables.net-bm';
+import DataTable from 'datatables.net-searchpanes';
+
+
+$.extend(true, DataTable.SearchPane.classes, {
+    disabledButton: 'is-disabled',
+    paneButton: 'button dtsp-paneButton is-white',
+    search: 'input search'
+});
+$.extend(true, DataTable.SearchPanes.classes, {
+    clearAll: 'dtsp-clearAll button',
+    collapseAll: 'dtsp-collapseAll button',
+    disabledButton: 'is-disabled',
+    showAll: 'dtsp-showAll button'
+});
+
+
+export default DataTable;
