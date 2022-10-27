55a8640960378eab7e46c9cdf1e27a902c1e3f87
Allan Jardine <allan@datatables.net>
2022-10-27 15:15:01 +0000

    Update typing information to latest typing integration for DataTables

---
+++ types/types.d.ts
@@ -0,0 +1,21 @@
+
+// Dist-DataTables-SearchPanes-Bulma integration with Bulma exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-SearchPanes-Bulma integration with Bulma exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
+
+// Dist-DataTables-SearchPanes-Bulma integration with Bulma exports the DataTables API having
+// set default values to complete the ingeration.
+import Api from "datatables.net";
+
+export default Api;
+
